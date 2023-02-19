a1f80ef739e6d1593467da454d42b4cdc73f0569
Juan Faz <75135385+juancfaz@users.noreply.github.com>
2023-02-19 10:03:02 -0700

    main update

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -2,37 +2,41 @@
 #include "include/SM.h"
 #include <time.h>
 #include <iostream>
+#include "gnuplot-iostream.h"
 
-double time_execution(void (*sortmethod)(int*, int), int* arr, int n) {
-    clock_t start_time, end_time;
-    // Registra el tiempo de inicio
-    start_time = clock();
-    // Ejecuta el método de ordenamiento
-    sortmethod(arr, n);
-    // Registra el tiempo de fin
-    end_time = clock();
-    // Calcula el tiempo de ejecución en milisegundos
-    double execution_time = (double) (end_time - start_time) * 1000 / CLOCKS_PER_SEC;
-    return execution_time;
+using namespace std;
+
+long long time_execution(void (*sort_function)(int*, int), int *arr, int n) {
+    auto start_time = chrono::high_resolution_clock::now();
+
+    sort_function(arr, n);
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+    return duration.count();
 }
 
-double time_execution_g(void (*sortmethod)(int*, int, int), int* arr, int left, int right) {
-    clock_t start_time, end_time;
-    // Registra el tiempo de inicio
-    start_time = clock();
-    // Ejecuta el método de ordenamiento
-    sortmethod(arr, left, right);
-    // Registra el tiempo de fin
-    end_time = clock();
-    // Calcula el tiempo de ejecución en milisegundos
-    double execution_time = (double) (end_time - start_time) * 1000 / CLOCKS_PER_SEC;
-    return execution_time;
-}
+long long time_execution_g(void (*sort_function)(int*, int, int), int *arr, int left, int right) {
+    auto start_time = chrono::high_resolution_clock::now();
 
+    sort_function(arr, left, right);
+
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+    return duration.count();
+}
 
 int main() {
-    std::srand(std::time(nullptr));
-    const int n = 10000; // Tamaño de la lista de elementos
+    Gnuplot gp;
+    gp << "plot sin(x)\n";
+    //plot_complexity();
+    return 0;
+}
+    /*
+    srand(time(nullptr));
+    const int n = 5; // Tamaño de la lista de elementos
 
     int arr_best[n]; // Lista para el mejor caso
     for (int i = 0; i < n; i++) {
@@ -42,7 +46,7 @@ int main() {
     int arr_avg[n];
     for (int i = 0; i < n; i++)
     {
-        arr_avg[i] = std::rand() % n + 1;
+        arr_avg[i] = rand() % n + 1;
     }
 
     int arr_worst[n]; // Lista para el peor caso
@@ -51,22 +55,22 @@ int main() {
     }
 
     // Ordenamiento por inserción
-    std::cout << "Insercion - mejor caso: " << time_execution(insertionSort, arr_best, n) << " ms" << std::endl;
-    std::cout << "Insercion - caso promedio: " << time_execution(selectionSort, arr_avg, n) << " ms" << std::endl;
-    std::cout << "Insercion - peor caso: " << time_execution(insertionSort, arr_worst, n) << " ms" << std::endl << std::endl;
+    std::cout << "Insercion - mejor caso: " << time_execution(insertionSort, arr_best, n) << " ms" << endl;
+    std::cout << "Insercion - caso promedio: " << time_execution(selectionSort, arr_avg, n) << " ms" << endl;
+    std::cout << "Insercion - peor caso: " << time_execution(insertionSort, arr_worst, n) << " ms" << endl << endl;
 
     // Ordenamiento por selección
-    std::cout << "Seleccion - mejor caso: " << time_execution(selectionSort, arr_best, n) << " ms" << std::endl;
-    std::cout << "Seleccion - caso promedio: " << time_execution(selectionSort, arr_avg, n) << " ms" << std::endl;
-    std::cout << "Seleccion - peor caso: " << time_execution(selectionSort, arr_worst, n) << " ms" << std::endl << std::endl;
+    std::cout << "Seleccion - mejor caso: " << time_execution(selectionSort, arr_best, n) << " ms" << endl;
+    std::cout << "Seleccion - caso promedio: " << time_execution(selectionSort, arr_avg, n) << " ms" << endl;
+    std::cout << "Seleccion - peor caso: " << time_execution(selectionSort, arr_worst, n) << " ms" << endl << endl;
 
     // Ordenamiento por mezcla
-    std::cout << "Merge - mejor caso: " << time_execution_g(mergeSort, arr_avg, 0, n - 1) << " ms" << std::endl;
-    std::cout << "Merge - peor caso: " << time_execution_g(mergeSort, arr_avg, 0, n - 1) << " ms" << std::endl;
+    std::cout << "Merge - mejor caso: " << time_execution_g(mergeSort, arr_avg, 0, n - 1) << " ms" << endl;
+    std::cout << "Merge - caso promedio: " << time_execution_g(mergeSort, arr_avg, 0, n - 1) << " ms" << endl;
+    std::cout << "Merge - peor caso: " << time_execution_g(mergeSort, arr_avg, 0, n - 1) << " ms" << endl;
 
-    std::cout << std::endl << "Tests" << std::endl;
+    cout << endl << "Tests" << endl;
     test_random_selection();
     test_random_insert();
     test_random_merge();
-    return 0;
-}
+    */
